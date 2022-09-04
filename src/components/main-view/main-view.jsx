a2a783d8614d2d5e8c0959c94b620541a8ede1b9
Priyanka-BN11 <priyankabnpinky@gmail.com>
2022-09-04 22:48:05 +0530

    class components lifecycle methods used

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,33 @@
 //Import react into file
 import React from 'react';
+import axios from 'axios';
 import {MovieCard} from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 //Exporting the MainView component makes it available for use by other components, modules, and files
 class MainView extends React.Component {
     constructor(){
         super();
+         // code executed right when the component is created in the memory
         this.state = {
-            movies: [ 
-            { _id:1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg'},
-            { _id:2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg'},
-            { _id:3, Title: 'Joker', Description: 'A mentally troubled stand-up comedian embarks on a downward spiral that leads to the creation of an iconic villain.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/Joker_%282019_film%29_poster.jpg/220px-Joker_%282019_film%29_poster.jpg'}
-        ],
+            movies: [],
             selectedMovie: null
         };
+    }
+    componentDidMount(){
+    // code executed right after the component is added to the DOM.
+        axios.get('https://movie-app-priya.herokuapp.com/movies')
+        .then(response => {
+            this.setState({
+                movies:response.data
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    componentWillUnmount(){
+    // code executed just before the moment the component gets removed from the DOM.
+
     }
     setSelectedMovie(newSelectedMovie){
         this.setState({
@@ -24,14 +38,14 @@ class MainView extends React.Component {
         const { movies, selectedMovie} = this.state;
         // if(selectedMovie) return <MovieView movie = {selectedMovie}/>;
         if(movies.length === 0) return <div className="main-view">The list is empty</div>; 
-        //Displays
+        //Displays movie list
         //JSX
         return (
             <div className="main-view">
             {selectedMovie
               ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
               : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
               ))
             }
           </div>
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 export class MovieView extends React.Component{
+    keypressCallback(event){
+        console.log(event.key);
+    }
+    componentDidMount(){
+        document.addEventListener('keypress', this.keypressCallback);
+    }
+    //removes the event listener when the component is about to be unmounted
+    componentWillUnmount(){
+        document.removeEventListener('keypress',this.keypressCallback);
+    }
     render(){
         const {movie, onBackClick} = this.props;
         return(
@@ -8,12 +18,25 @@ export class MovieView extends React.Component{
                     <img src ={movie.ImagePath}/> 
                 </div>
                 <div className="movie-title">
-                    <span className="label">Title:</span>
+                    <span className="label">Title: </span>
                     <span className="value">{movie.Title}</span>
                 </div>
                 <div className="movie-description">
                     <span className="label">Description: </span>
                     <span className="value">{movie.Description}</span>
+                </div>
+                <div className="movie-genre">
+                    <span className="label">Genre: </span>
+                    <span className="value">{movie.Genre.Name}</span>
+                </div>
+                <div className="movie-director">
+                    <span className="label">Director: </span>
+                    <span className="value">{movie.Director.Name}</span>
+                </div>
+                <div className="movie-actors">
+                    <span className="label">Actors: </span>
+                    <span className="value">{movie.Actors}</span>
+
                 </div>
                 <button onClick={() => {onBackClick(null); }}>Back</button>
             </div>
